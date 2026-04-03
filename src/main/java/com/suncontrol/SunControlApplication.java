b7d72134dc2d2fe798bde30aa1565ad3435a410a
callaing0 <0hmong0212@gmail.com>
2026-04-03 09:40:12 +0900

    Test

---
+++ src/main/java/com/suncontrol/SunControlApplication.java
@@ -0,0 +1,13 @@
+package com.suncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suncontrol/suncontrol/SunControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suncontrol.suncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
