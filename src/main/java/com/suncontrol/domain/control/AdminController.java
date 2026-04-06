f4ed99d0505c5b134c0495575360f22d82fe7647
callaing0 <0hmong0212@gmail.com>
2026-04-06 16:40:06 +0900

    대시보드, 통계, 마이페이지, 관리자페이지, 헤더, 사이드바, 모달 html 작성

--- src/main/java/com/suncontrol/domain/control/AdminController.java
+++ src/main/java/com/suncontrol/domain/control/AdminController.java
@@ -3,6 +3,7 @@ package com.suncontrol.domain.control;
 import com.suncontrol.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,8 +15,9 @@ public class AdminController {
     private final MemberService memberService;
 
     // 관리자 페이지 화면 반환
-    @GetMapping("/")
-    public String showAdminPage() {
-        return "admin"; // 관리자페이지 html 과 이름 맞는지 확인 부탁드립니다!
+    @GetMapping({"", "/"})
+    public String showAdminPage(Model model) {
+        model.addAttribute("userList", memberService.findAll());
+        return "admin";
     }
-}
+    }
