0ae0d6879dd5c1535c5cf8078171aee5760f9552
callaing0 <0hmong0212@gmail.com>
2026-04-10 11:13:46 +0900

    관리자 페이지 발전 수동 데이터 생성 수정

--- src/main/java/com/suncontrol/domain/control/AdminController.java
+++ src/main/java/com/suncontrol/domain/control/AdminController.java
@@ -1,5 +1,6 @@
 package com.suncontrol.domain.control;
 
+import com.suncontrol.core.service.asset.PlantService;
 import com.suncontrol.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     private final MemberService memberService;
+    private final PlantService plantService;
 
     // 관리자 페이지 화면 반환
     @GetMapping({"", "/"})
     public String showAdminPage(Model model) {
         model.addAttribute("userList", memberService.findAll());
+        //관리자 페이지 발전 수동 데이터 발전소 선택을 위한 추가 코드
+        model.addAttribute("plantList", plantService.findAllActive());
         return "admin";
     }
     }
