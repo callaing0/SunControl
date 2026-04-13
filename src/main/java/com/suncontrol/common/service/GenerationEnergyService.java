243a5e1ecaf34c3fa28ca7db37101a7d47c09414
mingundev12 <mingundev12@gmail.com>
2026-04-13 10:13:36 +0900

    Update GenerationEnergyService.java

--- src/main/java/com/suncontrol/common/service/GenerationEnergyService.java
+++ src/main/java/com/suncontrol/common/service/GenerationEnergyService.java
@@ -193,7 +193,7 @@ public class GenerationEnergyService {
     private GenerationResultSet getResult
             (LocalDateTime start, LocalDateTime end, InverterGenerationDto inv,
              WeatherContext context, int termSecond) {
-        log.info("{}인버터 {}부터 {}까지의 기록생성", inv.getId(), start, end);
+        log.debug("{} 인버터 {}부터 {}까지의 기록생성", inv.getId(), start, end);
         List<GenerationResultDto> resultList = new ArrayList<>();
         LocalDateTime current = start;
 
